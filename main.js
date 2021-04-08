49a291df9ddb83ba716fe3e0c4098f8c3a4abb04
Chiron <theo.chiron26@gmail.com>
2021-04-08 13:49:01 +0200

    nodemon added

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const http = require('http');
 const app = require('./server');
 
-app.set('port', process.env.PORT || 3000);
-
-
+app.set('port', process.env.PORT || 3000); //defini le port
 
 const server = http.createServer(app);
 
 
+server.listen(process.env.PORT || 3000); //ecouter sur le port 3000
 
-
-server.listen(process.env.PORT || 3000);
+//https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466277-creez-une-application-express
