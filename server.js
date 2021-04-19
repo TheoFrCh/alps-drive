841cd427a8a8f4598d989a32a789cd89fed60d0e
Theo <theo.chiron26@gmail.fr>
2021-04-19 09:57:23 +0200

    feat etape 5 complete

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requêtes a bien été reçue !' }); 
-});
+app.use(express.static('frontend'));
 
 module.exports = app;
